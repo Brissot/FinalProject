b93c796c57fdcc48562308664f14e7688eaf3c1f
Naaman Trumbull <trumbull@terpmail.umd.edu>
2024-05-15 21:09:13 -0400

    Added changes

--- cfbRequests.js
+++ cfbRequests.js
@@ -1,82 +1,82 @@
 const cfb = require('cfb.js');
 
 class cfbRequests {
-    #defaultClient;
-    #ApiKeyAuth;
+	#defaultClient;
+	#ApiKeyAuth;
 
-    #bettingApi;
-    #drivesApi;
+	#bettingApi;
+	#drivesApi;
 
-    constructor(apiKey) {
-	this.defaultClient = cfb.ApiClient.instance;
+	constructor(apiKey) {
+		this.defaultClient = cfb.ApiClient.instance;
 
-	/* key authorization */
-	this.ApiKeyAuth = this.defaultClient.authentications['ApiKeyAuth'];
-	this.ApiKeyAuth.apiKey = `Bearer ${apiKey}`;
+		/* key authorization */
+		this.ApiKeyAuth = this.defaultClient.authentications['ApiKeyAuth'];
+		this.ApiKeyAuth.apiKey = `Bearer ${apiKey}`;
 
-	/* initialize the apis */
-	this.bettingApi = new cfb.BettingApi();
-	this.drivesApi = new cfb.DrivesApi();
-	this.matchupApi = new cfb.TeamsApi();
-    }
-
-    async getBets(year, team) {
-	const opts = { year: year, team: team, seasonType: "both" };
-
-	try {
-	    const rawData = await this.bettingApi.getLines(opts);
-
-	    rawData.sort((first, second) => first.week - second.week);
-
-	    const headers= ["Week", "Season Type",
-			    "Start Date", "Home Team",
-			    "Home Conference", "Home Score",
-			    "Away Team", "Away Conference",
-			    "Away Score"];
-	    const internalHeaders= ['week',           'seasonType',
-				    'startDate',      'homeTeam',
-				    'homeConference', 'homeScore',
-				    'awayTeam',       'awayConference',
-				    'awayScore'];
-	    
-	    const data= await this.formatBets(headers, internalHeaders, rawData);
-
-	    return { headers, internalHeaders, data };
-	}
-	catch (error) {
-	    throw ("failed to retreive from betting api", error);
-	}
-    }
-    
-    async formatBets(headers, internalHeaders, rawData) {
-	let dict= {};
-	for (let header of internalHeaders) {
-	    dict[header]= []; /* initialize with empty dictionaries */
-	}
-	
-	for (let rawDataDict of rawData) {
-	    for (let header of internalHeaders) {
-		dict[header]= dict[header].concat([rawDataDict[header]]);
-	    }
+		/* initialize the apis */
+		this.bettingApi = new cfb.BettingApi();
+		this.drivesApi = new cfb.DrivesApi();
+		this.matchupApi = new cfb.TeamsApi();
 	}
 
-	return dict;
-    }
+	async getBets(year, team) {
+		const opts = { year: year, team: team, seasonType: "both" };
 
-    
-    async getDrives(year, team, week) {
-	const opts = { conference: "Big Ten", week: week };
+		try {
+			const rawData = await this.bettingApi.getLines(opts);
 
-	try {
-	    let data = await this.drivesApi.getDrives(56);
+			rawData.sort((first, second) => first.week - second.week);
 
-	    // console.log(data);
-	    return data;
+			const headers = ["Week", "Season Type",
+				"Start Date", "Home Team",
+				"Home Conference", "Home Score",
+				"Away Team", "Away Conference",
+				"Away Score"];
+			const internalHeaders = ['week', 'seasonType',
+				'startDate', 'homeTeam',
+				'homeConference', 'homeScore',
+				'awayTeam', 'awayConference',
+				'awayScore'];
+
+			const data = await this.formatBets(headers, internalHeaders, rawData);
+
+			return { headers, internalHeaders, data };
+		}
+		catch (error) {
+			throw ("failed to retreive from betting api", error);
+		}
 	}
-	catch (error) {
-	    throw ("failed to retreive from drives api", error);
+
+	async formatBets(headers, internalHeaders, rawData) {
+		let dict = {};
+		for (let header of internalHeaders) {
+			dict[header] = []; /* initialize with empty dictionaries */
+		}
+
+		for (let rawDataDict of rawData) {
+			for (let header of internalHeaders) {
+				dict[header] = dict[header].concat([rawDataDict[header]]);
+			}
+		}
+
+		return dict;
 	}
-    }
+
+
+	async getDrives(year, team, week) {
+		const opts = { conference: "Big Ten", week: week };
+
+		try {
+			let data = await this.drivesApi.getDrives(56);
+
+			// console.log(data);
+			return data;
+		}
+		catch (error) {
+			throw ("failed to retreive from drives api", error);
+		}
+	}
 
 
 	async getMatchups(team1, team2) {
@@ -108,7 +108,7 @@ class cfbRequests {
 			for (let key of Object.keys(dictionary)) {
 				for (let game of rawData) {
 					dictionary[key] = dictionary[key] != [] ?
-					  [...dictionary[key], game[key]] : [game[key]];
+						[...dictionary[key], game[key]] : [game[key]];
 				}
 			}
 
